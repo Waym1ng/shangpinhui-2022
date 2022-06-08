ae853ddb2523b61eec4067308fd0bab5a2ca52e9
zhangmingwei <wb.zhangmingwei01@mesg.corp.netease.com>
2022-06-08 14:26:28 +0800

    静态页面组件的拆分

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import TypeNav from '@/pages/Home/TypeNav'
+// 三级联动全局组件
+Vue.component(TypeNav.name, TypeNav)
 
 Vue.config.productionTip = false
 
