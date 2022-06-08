83aefe53f292202d90e6618c434f4b40ce710d7f
zhangmingwei <wb.zhangmingwei01@mesg.corp.netease.com>
2022-06-08 17:27:42 +0800

    商品导航-三级分类

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,10 +1,26 @@
-const state = { "home": 1 };
-const mutations = {};
-const actions = {};
+import { reqCategoryList } from "@/api";
+
+const state = {
+	categoryList: [],
+};
+const mutations = {
+	CATEGORYLIST(state, categoryList) {
+		state.categoryList = categoryList;
+	},
+};
+const actions = {
+	async categoryList({commit}) {
+		let results = await reqCategoryList();
+		if (results.code == 200) {
+      // console.log(results);
+			commit("CATEGORYLIST", results.data);
+		}
+	},
+};
 const getters = {};
 export default {
-  state,
-  mutations,
-  actions,
-  getters,
+	state,
+	mutations,
+	actions,
+	getters,
 };
